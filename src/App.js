a07ea3406fb6bb3025c29ed0a6b558100ec32470
정서연 <jjungseoyeon@naver.com>
2022-03-10 16:48:53 +0900

    faker.js 추가 실습

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
+import { faker } from '@faker-js/faker';
 import dog from './dog.jpg';
 import './App.css';
 
+//JSX
+const testData = [
+  {
+    text: "대통령은 헌법과 법률이 정하는 바에 의하여 공무원을 임면한다.",
+    imgUrl: "https://file.thisisgame.com/upload/tboard/user/2013/06/25/20130625165342_3182.jpg"
+  },{
+    text: "국회는 법률에 저촉되지 아니하는 범위안에서 의사와 내부규율에 관한 규칙을 제정할 수 있다.",
+    imgUrl: "https://file.thisisgame.com/upload/tboard/user/2013/06/25/20130625165343_9591.jpg"
+  }, {
+    text: "의원을 제명하려면 국회재적의원 3분의 2 이상의 찬성이 있어야 한다.",
+    imgUrl: "https://file.thisisgame.com/upload/tboard/user/2013/06/25/20130625165332_3141.jpeg"
+  }
+]
+
 function App() {
+  const h1Element = <h1>H1 제목 태그 입니다.</h1>;
+  const imgElemnet = <img src={dog} className="App-logo" alt="logo" />;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={dog} className="App-logo" alt="logo" />
+        { h1Element }
+        { imgElemnet }
         <p>
           히히 <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {testData.map((contents)=>{
+            return <div>
+              <img src={faker.image.avatar()} alt="강아지 사진" />
+              {contents.text}
+              <img src={faker.image.cats()} alt="강아지 사진" />
+              </div>
+          })}
+        </ul>
       </header>
     </div>
   );
