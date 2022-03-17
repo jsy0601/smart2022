82a825a9f4bbb80fec4298179e29e7541a5ad8bd
정서연 <jjungseoyeon@naver.com>
2022-03-17 16:47:07 +0900

    Grid 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import faker from '@faker-js/faker';  //영문 버전의 fakerjs
 import faker_ko from '@faker-js/faker/locale/ko';  //한글 버전의 fakerjs
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import './App.css';
+import { Grid } from '@mui/material';
+import Container from '@mui/material/Container';
+import UserCard from './components/UserCard';
 
-
-function App() {
   const userDatas = [];
 
   while(userDatas.length < 5){
@@ -22,43 +16,20 @@ function App() {
     })
   }
 
+function App() {
   const userCards = userDatas.map((userData, idx) => {
-    return <Card sx={{ maxWidth: 345 }}>
-    <CardMedia
-      component="img"
-      height="345"
-      image={ userData.avatar }
-      alt="green iguana"
-    />
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        <h4>{ userData.jobTitle }</h4>
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        <h5>{ userData.name }</h5>
-        {userData.email} <br />
-        {userData.phoneNo}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Share</Button>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
-    // <div key={idx}>
-    //   <h4>{ userData.jobTitle }</h4>
-    //   <img src={ userData.avatar } alt="사용자 프로필용 아바타"></img>
-    //   <h5>{ userData.name }</h5>
-    //   {userData.email} <br />
-    //   {userData.phoneNo}
-    // </div>
+    return <Grid item xs={2} sm={4} md={4} key={idx}>
+       <UserCard userData={userData} />;
+      </Grid>
   })
 
   console.log(userDatas)
   return (
-    <div className="App">
-        {userCards}
-    </div>
+    <Container maxWidth="lg" sx={{p:1}}>
+      <Grid container spacing={{ xs: 2, md: 3}} columns={{ xs: 4, sm: 8, md: 12}}>
+          {userCards}
+      </Grid>
+    </Container>
   );
 }
 
