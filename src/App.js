a12998d125038cdef7a4f4fc9ae4f39ea30b7eca
정서연 <jjungseoyeon@naver.com>
2022-03-24 14:53:28 +0900

    다크모드 스위치 만들기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import faker_ko from '@faker-js/faker/locale/ko';  //한글 버전의 fakerjs
 import { Grid } from '@mui/material';
 import Container from '@mui/material/Container';
 import UserCard from './components/UserCard';
+import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
+import { useEffect } from 'react';
+import Box from '@mui/material/Box';
+import './App.css';
+import React, { useState } from 'react';
+
 
   const userDatas = [];
 
@@ -17,19 +24,53 @@ import UserCard from './components/UserCard';
   }
 
 function App() {
+  const [useDarkMode, setUseDarkMode] = useState(true);
+
+  const handleChange = (event) => {
+    setUseDarkMode(event.target.checked);
+  }
+
+  // useEffect(() => {
+  //   console.log("component did mount")
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log(`theme 변경됨 -> ${useDarkMode}`)
+  // }, [useDarkMode]);
+
   const userCards = userDatas.map((userData, idx) => {
     return <Grid item xs={2} sm={4} md={4} key={idx}>
        <UserCard userData={userData} />;
       </Grid>
   })
 
-  console.log(userDatas)
+  // console.log("render")
   return (
-    <Container maxWidth="lg" sx={{p:1}}>
-      <Grid container spacing={{ xs: 2, md: 3}} columns={{ xs: 4, sm: 8, md: 12}}>
-          {userCards}
-      </Grid>
-    </Container>
+    <ThemeProvider theme={createTheme({
+        palette: {
+          mode: useDarkMode ? 'dark' : 'light',
+        },
+      })
+    }>
+      <Box sx={{
+        height: '100%',
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        p: 1,
+      }}>
+        <Switch
+          checked={useDarkMode}
+          onChange={handleChange}
+          color="secondary"
+          inputProps={{ 'aria-label': 'controlled' }}
+          />
+        <Container maxWidth="lg" sx={{p:1}}>
+          <Grid container spacing={{ xs: 2, md: 3}} columns={{ xs: 4, sm: 8, md: 12}}>
+              {userCards}
+          </Grid>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
