365e69b1c5682768294de9b9951972d85b27545e
정서연 <jjungseoyeon@naver.com>
2022-03-31 22:27:54 +0900

    openweathermap API 적용하기

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
 import Switch from '@mui/material/Switch';
 import Box from '@mui/material/Box';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import WeatherCard from './components/WeatherCard';
 import UserCardList from './components/UserCardList';
 import { makeUserDatas } from './Utils';
 
@@ -11,10 +13,30 @@ const userDatas = makeUserDatas(5000);
 
 function App() {
   const [useDarkMode, setUseDarkMode] = useState(true);
+  const [weatherData, setWeathreData] = useState(null);
+  const [apiError, setApiError] = useState(null);
   
   const handleChange = (event) => {
     setUseDarkMode(event.target.checked);
   }
+
+  useEffect(() => {
+    const callApi = async() =>{
+      try{
+        const result = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat=37.394&lon=126.9568&appid=5bb29823a904b3493af4b2f66218b43c&lang=kr&units=metric&appid=5bb29823a904b3493af4b2f66218b43c")
+        setWeathreData(result.data)
+      }catch(err){
+        setApiError(err)
+      }
+    }
+    callApi();
+    console.log("component did mount");
+  },[]);
+
+  useEffect(() => {
+    console.log(`theme 변경됨 -> ${useDarkMode}`)
+  },[useDarkMode]);
+
 
   return (
     <ThemeProvider theme={createTheme({
@@ -23,6 +45,13 @@ function App() {
         },
       })
     }>
+      <Box sx={{
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        p: 1,
+      }}>
+        <WeatherCard weatherData={weatherData} apiError={apiError}/>
+      </Box>
       <Box sx={{
         height: '100%',
         bgcolor: 'background.default',
