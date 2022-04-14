fa60fb7198308aa9cb17bdfc7a87966b509f9062
정서연 <jjungseoyeon@naver.com>
2022-04-14 13:57:03 +0900

    사용되지 않는 import 지우기

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import { weather_mapping_data } from "../dataset/WeatherData";
 import { Grid } from '@mui/material';
 
